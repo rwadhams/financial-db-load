839b6576842c4373a2c50bf731dea5742ee716cd
rober <rober@Rob-Molly-Laptop>
2020-01-06 23:05:06 +1000

    Data Value Reporting

---
+++ FinancialDBLoad/src/main/groovy/com/wadhams/financials/db/load/service/DataValueReportingService.groovy
@@ -0,0 +1,97 @@
+package com.wadhams.financials.db.load.service
+
+import java.text.SimpleDateFormat
+
+import com.wadhams.financials.db.load.dto.FinancialDTO
+import com.wadhams.financials.db.load.type.Asset
+import com.wadhams.financials.db.load.type.Category
+import com.wadhams.financials.db.load.type.SubCategory
+import groovy.sql.Sql
+
+class DataValueReportingService {
+	SQLBuilderService sqlBuilderService = new SQLBuilderService()
+	
+	def reportDistinctValues(PrintWriter pw) {
+		Sql sql = Sql.newInstance('jdbc:h2:~/financial', 'sa', '', 'org.h2.Driver')
+		
+		String categorySubCategoryQuery = sqlBuilderService.buildDistinctCategorySubCategorySelect()
+		println categorySubCategoryQuery
+		println ''
+		
+		pw.println 'Distinct Category/SubCategory Values'
+		pw.println '------------------------------------'
+		String category = ''
+		sql.eachRow(categorySubCategoryQuery) {row ->
+			String c01 = row.CAT
+			String c02 = row.SUBCAT
+			if (category != c01) {
+				category = c01
+				pw.println c01
+			}
+			if (c02) {
+				pw.println "\t$c02"
+			}
+		}
+		pw.println ''
+		
+		String assetQuery = sqlBuilderService.buildAssetSelect()
+		println assetQuery
+		println ''
+		pw.println 'Asset Category SubCategory'
+		pw.println '--------------------------'
+		sql.eachRow(assetQuery) {row ->
+			String c01 = row.PAYEE
+			String c02 = row.DESC
+			String c03 = row.ASSET
+			String c04 = row.CAT
+			String c05 = row.SUBCAT
+			pw.println "$c03|$c04|$c05\t\t$c01, $c02"
+		}
+		pw.println ''
+
+		String specificRunningCostQuery = sqlBuilderService.buildSpecificRunningCostSelect()
+		println specificRunningCostQuery
+		println ''
+		pw.println 'Specific Running Costs (Asset|Category)'
+		pw.println '---------------------------------------'
+		sql.eachRow(specificRunningCostQuery) {row ->
+			String c01 = row.PAYEE
+			String c02 = row.DESC
+			String c03 = row.ASSET
+			String c04 = row.CAT
+			String c05 = row.START
+			String c06 = row.END
+			pw.println "$c03|$c04|$c05|$c06\t\t$c01, $c02"
+		}
+		pw.println ''
+
+		String ongoingRunningCostQuery = sqlBuilderService.buildOngoingRunningCostSelect()
+		println ongoingRunningCostQuery
+		println ''
+		pw.println 'Ongoing Running Costs (Asset|Category)'
+		pw.println '--------------------------------------'
+		sql.eachRow(ongoingRunningCostQuery) {row ->
+			String c01 = row.PAYEE
+			String c02 = row.DESC
+			String c03 = row.ASSET
+			String c04 = row.CAT
+			String c05 = row.START
+			String c06 = row.END
+			pw.println "$c03|$c04|$c05|$c06\t\t$c01, $c02"
+		}
+		pw.println ''
+
+		String reportGrouping1Query = sqlBuilderService.buildDistinctReportGrouping1Select()
+		println reportGrouping1Query
+		println ''
+		pw.println 'Distinct Report Grouping 1'
+		pw.println '--------------------------'
+		sql.eachRow(reportGrouping1Query) {row ->
+			String c01 = row.RG1
+			pw.println c01
+		}
+		pw.println ''
+		
+	}
+	
+}
--- FinancialDBLoad/src/main/groovy/com/wadhams/financials/db/load/service/SQLBuilderService.groovy
+++ FinancialDBLoad/src/main/groovy/com/wadhams/financials/db/load/service/SQLBuilderService.groovy
@@ -109,4 +109,59 @@ class SQLBuilderService {
 		return sb.toString()
 	}
 	
+	String buildDistinctCategorySubCategorySelect() {
+		StringBuilder sb = new StringBuilder()
+		
+		sb.append('SELECT DISTINCT CATEGORY AS CAT, SUB_CATEGORY AS SUBCAT ')
+		sb.append('FROM FINANCIAL ')
+		sb.append('ORDER BY 1,2')
+		
+		return sb.toString()
+	}
+	
+	String buildDistinctReportGrouping1Select() {
+		StringBuilder sb = new StringBuilder()
+		
+		sb.append('SELECT DISTINCT RPT_GRP_1 AS RG1 ')
+		sb.append('FROM FINANCIAL ')
+		sb.append('WHERE RPT_GRP_1 IS NOT NULL ')
+		sb.append('ORDER BY 1')
+		
+		return sb.toString()
+	}
+	
+	String buildAssetSelect() {
+		StringBuilder sb = new StringBuilder()
+		
+		sb.append('SELECT PAYEE AS PAYEE, DESCRIPTION AS DESC, ASSET AS ASSET, CATEGORY AS CAT, SUB_CATEGORY AS SUBCAT ')
+		sb.append('FROM FINANCIAL ')
+		sb.append('WHERE ASSET IS NOT NULL ')
+		sb.append('AND RPT_GRP_1 IS NULL ')
+		sb.append('ORDER BY ASSET, CATEGORY, SUB_CATEGORY')
+		
+		return sb.toString()
+	}
+	
+	String buildSpecificRunningCostSelect() {
+		StringBuilder sb = new StringBuilder()
+		
+		sb.append('SELECT PAYEE AS PAYEE, DESCRIPTION AS DESC, ASSET AS ASSET, CATEGORY AS CAT, START_DT AS START, END_DT AS END ')
+		sb.append('FROM FINANCIAL ')
+		sb.append('WHERE RPT_GRP_1 = \'SPECIFIC_RUNNING_COST\' ')
+		sb.append('ORDER BY ASSET, CATEGORY, START_DT')
+		
+		return sb.toString()
+	}
+	
+	String buildOngoingRunningCostSelect() {
+		StringBuilder sb = new StringBuilder()
+		
+		sb.append('SELECT PAYEE AS PAYEE, DESCRIPTION AS DESC, ASSET AS ASSET, CATEGORY AS CAT, START_DT AS START, END_DT AS END ')
+		sb.append('FROM FINANCIAL ')
+		sb.append('WHERE RPT_GRP_1 = \'ONGOING_RUNNING_COST\' ')
+		sb.append('ORDER BY ASSET, CATEGORY, START_DT')
+		
+		return sb.toString()
+	}
+	
 }
